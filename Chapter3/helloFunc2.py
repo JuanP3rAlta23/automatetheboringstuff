9793d97e4d83326bd67fc0e6b1a52d778ba483aa
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2021-11-19 14:11:35 -0500

    Updates

--- Chapter3/helloFunc2.py
+++ Chapter3/helloFunc2.py
@@ -1,6 +1,10 @@
-
+# The definition of the hello() function in this program has a parameter called name. 
+# Parameters are variables that contain arguments. 
+# When a function is called with arguments, the arguments are stored in the parameters.
 def hello(name):
    print('Hello, ' + name)
-
+#The first time the hello() function is called, it is passed the argument 'Juan'.
+#The program execution enters the function, and the parameter name is automatically set to 'Juan', 
+# which is what gets printed by the print() statement 
 hello('Juan')
 hello('Rita')
