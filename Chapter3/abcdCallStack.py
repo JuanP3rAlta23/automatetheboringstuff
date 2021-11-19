cff2499d286b9b6eaf21168a8b87903a8a08beee
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2021-11-19 15:30:59 -0500

    Update abcdCallStack.py

--- Chapter3/abcdCallStack.py
+++ Chapter3/abcdCallStack.py
@@ -1,5 +1,10 @@
-
-
+# When a() is called , it calls b(), which in turn calls c(). 
+# The c() function doesn’t call anything; it just displays c() starts and c() returns before returning to the line in b() that called it.
+# Once execution returns to the code in b() that called c(), it returns to the line in a() that called b().
+# The execution continues to the next line in the b() function, which is a call to d().
+# Like the c() function, the d() function also doesn’t call anything. It just displays d() starts and d() returns before returning to the line in b() that called it. 
+# Since b() contains no other code, the execution returns to the line in a() that called b().
+# The last line in a() displays a() returns before returning to the original a() call at the end of the program.
 
 def a():
     print('a() starts') 
