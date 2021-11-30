ba397163b6c0f2cbab3c9e8d3b0ed13fe66c78f4
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2021-11-29 22:27:13 -0500

    Update swordfish.py

--- Chapter2/swordfish.py
+++ Chapter2/swordfish.py
@@ -1,3 +1,7 @@
+# 'continue' statements are used inside loops. 
+# When the program execution reaches a continue statement, 
+# the program execution immediately jumps back to the start of the loop and reevaluates the loop’s condition. 
+# (This is also what happens when the execution reaches the end of the loop.)
 while True:
     print('Who are you?')
     name = input()
